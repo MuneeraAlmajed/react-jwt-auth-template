cab992cc8d6cab623e5bf8f737792fdbe27ad8c7
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-09-09 09:47:47 +0300

    used localstorage to persist user session

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,8 +4,27 @@ import { createContext } from 'react';
 
 const UserContext = createContext();
 
+const getUserFromToken= () =>{
+  //first pull the raw token in local storage
+      const token = localStorage.getItem("token");
+      
+      //if there is not token the user is not signed in
+      if (!token) return null;
+      
+      
+      
+      //then extract the payload
+      const payload = token.split(".")[1];
+
+      //convert the specialize dpayload into JSON
+      const tokenJson = atob(payload);
+
+      //take that josn and convert it back into json
+      return JSON.parse(tokenJson);
+}
+
 function UserProvider({ children }) {
-    const [user,setUser] = useState(null);
+    const [user,setUser] = useState(getUserFromToken);
 
     const value = { user, setUser};
     
